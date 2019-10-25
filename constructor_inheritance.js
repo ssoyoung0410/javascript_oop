56bc749bfbc89e59ca30f9bcc80d10dd817f7f6f
Lee So Young <cheerup0101@naver.com>
2019-10-25 20:06:57 +0900

    객체지향수업끝

--- constructor_inheritance.js
+++ constructor_inheritance.js
@@ -4,19 +4,26 @@ function Person(name,first,second){
     this.second = second;
 }
 
-Person.prototype.sum = function() { //Person을 통해 생성된 모든객체가 sum 사용가능.
+Person.prototype.sum = function() {  //Person을 통해 생성된 모든객체가 
+    //sum 사용가능. 
     return this.first + this.second;
 }
 
 function PersonPlus(name, first, second, third){
     Person.call(this, name, first, second, third);
+    //call안의 this는 PersonPlus함수를 통해서만들어질 객체
+
     this.third = third;
 }
-PersonPlus.prototype.__proto__ = Person.prototype;
-PersonPlus.prototype.avg = function(){
+
+PersonPlus.prototype = Object.create(Person.prototype);
+PersonPlus.prototype.constructor = PersonPlus;
+
+PersonPlus.prototype.avg= function(){
     return (this.first+this.second+this.third)/3
-}
+} 
 
 
 var kim = new PersonPlus('kim', 10, 20,30);
 console.log(kim.sum());
+console.log(kim.constructor); 
